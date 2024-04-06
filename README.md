23781dce2bc2525e0bc6c7d68d842864b664a787
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-04-06 22:08:48 +0200

    README and example code

---
+++ README.md
@@ -0,0 +1,21 @@
+# Sample application to go with slides for Pascal Cafe 2024 Conference (IJsselstein ,Netherlands)
+
+This is the sample application developed to show during _Pascal Cafe 2024_ conference.
+
+See the conference slides on https://castle-engine.io/pascalcafe2024 .
+
+See the conference details on
+- https://www.blaisepascalmagazine.eu/en/international-lazarus-conference-in-holland-april-2024/
+- https://castle-engine.io/wp/2024/03/15/register-for-pascal-cafe-in-ijsselstein-netherlands-on-april-6th-saturday/
+- https://castle-engine.io/conferences
+
+I did not actually use this application during the presentation -- because we managed to make "live" a similar but not exactly the same application during presentation. So it was even better :) Still the application in this repo is nice to read along with slides.
+
+## Authors, License
+
+Code by Michalis Kamburelis, public domain.
+
+Monster models from Quaternius:
+- https://quaternius.com/packs/ultimateanimatedanimals.html
+- license: public domain
+- Support the artist: https://www.patreon.com/quaternius
---
+++ physics-and-coding-demo/code/gameviewmain.pas
@@ -0,0 +1,73 @@
+{ Main view, where most of the application logic takes place.
+
+  Feel free to use this code as a starting point for your own projects.
+  This template code is in public domain, unlike most other CGE code which
+  is covered by BSD or LGPL (see https://castle-engine.io/license). }
+unit GameViewMain;
+
+interface
+
+uses Classes,
+  CastleVectors, CastleComponentSerialize,
+  CastleUIControls, CastleControls, CastleKeysMouse, CastleViewport;
+
+type
+  { Main view, where most of the application logic takes place. }
+  TViewMain = class(TCastleView)
+  published
+    { Components designed using CGE editor.
+      These fields will be automatically initialized at Start. }
+    LabelFps: TCastleLabel;
+    Viewport1: TCastleViewport;
+  public
+    constructor Create(AOwner: TComponent); override;
+    procedure Start; override;
+    procedure Update(const SecondsPassed: Single; var HandleInput: Boolean); override;
+    function Press(const Event: TInputPressRelease): Boolean; override;
+  end;
+
+var
+  ViewMain: TViewMain;
+
+implementation
+
+uses SysUtils;
+
+{ TViewMain ----------------------------------------------------------------- }
+
+constructor TViewMain.Create(AOwner: TComponent);
+begin
+  inherited;
+  DesignUrl := 'castle-data:/gameviewmain.castle-user-interface';
+end;
+
+procedure TViewMain.Start;
+begin
+  inherited;
+end;
+
+procedure TViewMain.Update(const SecondsPassed: Single; var HandleInput: Boolean);
+begin
+  inherited;
+  { This virtual method is executed every frame (many times per second). }
+  Assert(LabelFps <> nil, 'If you remove LabelFps from the design, remember to remove also the assignment "LabelFps.Caption := ..." from code');
+  LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
+end;
+
+function TViewMain.Press(const Event: TInputPressRelease): Boolean;
+begin
+  Result := inherited;
+  if Result then Exit; // allow the ancestor to handle keys
+
+  if Event.IsMouseButton(buttonLeft) then
+  begin
+    if (Viewport1.TransformUnderMouse <> nil) and
+       (Viewport1.TransformUnderMouse.RigidBody <> nil) then
+      Viewport1.TransformUnderMouse.RigidBody.ApplyImpulse(
+        Vector3(0, 10, 0),
+        Viewport1.TransformUnderMouse.WorldTranslation);
+    Exit(true);
+  end;
+end;
+
+end.
